8171b60ee98600cc8411993256b2f58a28a46dd7
Kipngetich33 <khalifngeno@gmail.com>
2018-01-30 12:23:38 +0300

    add the image and profile models

--- instagram/gram/models.py
+++ instagram/gram/models.py
@@ -1,2 +1,13 @@
+from django.db import models
 
+class Image(models.Model):
+    image = models.ImageField(upload_to="images/")
+    image_name = models.CharField(max_length =30)
+    image_caption = models.TextField(blank=True)
+    profile_key = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to="images/")
+    bio = models.TextField(blank=True)
+    user_key = models.ForeignKey(User ,on_delete=models.CASCADE)
 
