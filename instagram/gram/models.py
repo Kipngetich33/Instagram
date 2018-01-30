b84546fae0870e24c95ba57c1930d3e599106b85
Kipngetich33 <khalifngeno@gmail.com>
2018-01-30 15:29:32 +0300

    create the firrst test to test the creation of image objects

--- instagram/gram/models.py
+++ instagram/gram/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 
-class Image(models.Model):
-    image = models.ImageField(upload_to="images/")
-    image_name = models.CharField(max_length =30)
-    image_caption = models.TextField(blank=True)
-    profile_key = models.ForeignKey(Profile, on_delete=models.CASCADE)
-
 class Profile(models.Model):
     profile_photo = models.ImageField(upload_to="images/")
     bio = models.TextField(blank=True)
-    user_key = models.ForeignKey(User ,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.bio 
+
+class Image(models.Model):
+    image = models.ImageField(upload_to="images/",blank = True)
+    image_name = models.CharField(max_length =30)
+    image_caption = models.TextField(blank=True)
+    profile_key = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True,blank=True, null= True)
+
+    def __str__(self):
+        return self.image_name 
+
+
 
--- instagram/gram/tests.py
+++ instagram/gram/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from . models import Image
 
-# Create your tests here.
+class ImageTestClass(TestCase):
+    '''
+    A class that test the Image class model
+    '''
+
+    def setUp(self):
+        '''
+        method that runs at the begginning of each test
+        '''
+        self.image = Image(image = 'image_url',image_name ='vin' , image_caption='hey there',)
+
+    def test_instance(self):
+        '''
+        method that test is image objects are instanciated correctly
+        '''
+        self.assertTrue(isinstance(self.image,Image)) 
+
