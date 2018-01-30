906e0d4e26593c04ee283cf7fd5ac531ae86a076
Kipngetich33 <khalifngeno@gmail.com>
2018-01-30 16:45:59 +0300

    add tests for save, delete ,update caption and get image by id methods

--- instagram/gram/models.py
+++ instagram/gram/models.py
@@ -11,11 +11,22 @@ class Image(models.Model):
     image = models.ImageField(upload_to="images/",blank = True)
     image_name = models.CharField(max_length =30)
     image_caption = models.TextField(blank=True)
-    profile_key = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    profile_key = models.ForeignKey(Profile, on_delete=models.CASCADE, null= True)
     pub_date = models.DateTimeField(auto_now_add=True,blank=True, null= True)
 
     def __str__(self):
         return self.image_name 
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete() 
+
+    def update_caption(self,new_caption):
+        self.image_caption = new_caption
+        self.save()
+
 
 
 
--- instagram/gram/tests.py
+++ instagram/gram/tests.py
@@ -18,3 +18,40 @@ class ImageTestClass(TestCase):
         '''
         self.assertTrue(isinstance(self.image,Image)) 
 
+    def test_save_image(self):
+        '''
+        method that test the save method of model image
+        '''
+        self.image.save_image()
+        all_images= Image.objects.all()
+        self.assertTrue(len(all_images)>0)
+
+    def test_delete_images(self):
+        '''
+        method that tests the delete_images method
+        '''
+        self.image.save_image()
+        new_image = Image(image = 'image_url2',image_name ='vin2' , image_caption='hey there2',)
+        new_image.save_image()
+        self.image.delete_image()
+        all_images = Image.objects.all()
+        self.assertTrue(len(all_images)==1)
+
+    def test_update_caption(self):
+        '''
+        method that tests the update caption
+        '''
+        self.image.save_image()
+        image = Image.objects.get(image ='image_url')
+        image.update_caption('new caption')
+        image = Image.objects.get(image ='image_url')
+        self.assertTrue(image.image_caption=='new caption')
+
+    def test_get_image_by_id(self):
+        '''
+        method that tests the get image by id function of image model
+        '''
+        pass
+
+
+
