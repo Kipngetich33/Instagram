554081cd7f5b2b7527024f88903db426914d5de9
Kipngetich33 <khalifngeno@gmail.com>
2018-02-03 11:01:08 +0300

    untracking

--- virtual/lib/python3.6/site-packages/like_button/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from django.views.generic import TemplateView
-
-urlpatterns = patterns(
-    '',
-    url(r'^channel.html$', TemplateView.as_view(), {
-        'template': 'channel.html'}),
-)
